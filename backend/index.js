0108577d58d5dfa6a2658a9ae9c7ff28294b205b
神尾拓馬 <balo@MacBook-Pro.local>
2021-08-22 23:41:34 +0900

    made backend

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MONGODB is connected"))
+  .catch((err) => console.error(err));
+
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
+
+app.listen(8800, () => {
+  console.log("backend is running");
+});
